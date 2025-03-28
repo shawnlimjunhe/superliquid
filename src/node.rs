b0cc84df18044f9c4dbd32a165f81b327dd8225e
shawnlimjunhe <shawnlimjunhe@gmail.com>
2025-03-28 22:18:29 +0800

    add logic to hash transaction and broadcast transactions upon receipt

--- src/node.rs
+++ src/node.rs
@@ -1,13 +1,26 @@
 use tokio::net::{ TcpListener, TcpStream };
+use std::collections::HashSet;
 use std::io::Result;
 use std::sync::{ Arc, Mutex };
 
 use crate::types::Transaction;
-use crate::message_protocol::{ self, Message };
+use crate::message_protocol::{ self, send_transaction, Message };
 
 struct Node {
+    addr: String,
     is_leader: bool,
     transactions: Vec<Transaction>,
+    seen_transactions: HashSet<[u8; 32]>,
+    peers: Vec<Node>, // For now, we skip peer discovery
+}
+
+impl Node {
+    pub fn get_peer_addresses(&self) -> Vec<String> {
+        self.peers
+            .iter()
+            .map(|node| node.addr.clone())
+            .collect()
+    }
 }
 
 pub async fn run_node(addr: &str) -> Result<()> {
@@ -15,8 +28,11 @@ pub async fn run_node(addr: &str) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     let node = Arc::new(
         Mutex::new(Node {
+            addr: addr.to_owned(),
             is_leader: true,
             transactions: vec![],
+            seen_transactions: HashSet::new(),
+            peers: vec![],
         })
     );
 
@@ -61,10 +77,28 @@ async fn handle_transaction(
 ) -> Result<()> {
     println!("Received Transaction: {:?}", tx);
     {
-        let mut node: std::sync::MutexGuard<'_, Node> = node.lock().expect("Lock failed");
-        node.transactions.push(tx);
+        let mut node = node.lock().expect("Lock failed");
+
+        if node.seen_transactions.insert(tx.hash()) {
+            node.transactions.push(tx.clone());
+        }
     }
-    message_protocol::send_ack(&mut socket).await
+    message_protocol::send_ack(&mut socket).await?;
+
+    let peer_addresses = {
+        let node = node.lock().expect("Lock failed");
+        node.get_peer_addresses()
+    };
+
+    for addr in peer_addresses.into_iter() {
+        let cloned_tx = tx.clone();
+        tokio::spawn(async move {
+            let mut stream = TcpStream::connect(addr).await?;
+            send_transaction(&mut stream, cloned_tx).await
+        });
+    }
+
+    Ok(())
 }
 
 async fn handle_query(mut socket: &mut TcpStream, node: &Arc<Mutex<Node>>) -> Result<()> {
--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,17 @@
 use serde::{ Serialize, Deserialize };
+use sha2::{ Digest, Sha256 };
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Transaction {
     pub from: String,
     pub to: String,
     pub amount: u64,
+}
+
+impl Transaction {
+    pub fn hash(&self) -> [u8; 32] {
+        // probably want to implement my own encoding and hashing
+        let encoded = bincode::serialize(&self).unwrap();
+        Sha256::digest(&encoded).into()
+    }
 }
