a1add93d8b0f760ee35d687cb12c5fbd2ac9abfe
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2025-06-03 00:48:36 +0800

    wip

--- src/state/transaction_delta.rs
+++ src/state/transaction_delta.rs
@@ -1,6 +1,6 @@
 use crate::types::transaction::PublicKeyHash;
 
-use super::asset::AssetId;
+use super::{asset::AssetId, order::Order};
 
 pub struct AssetDelta {
     pub account: PublicKeyHash,
@@ -20,4 +20,18 @@ pub struct SpotCancelOrderDelta {
     pub(crate) account_order_position: usize,
     pub(crate) order_level_index: usize,
     pub(crate) order_index: usize,
+}
+
+pub struct OrderBookDelta {}
+
+pub struct OrderDelta {
+
+}
+
+pub struct SpotOrderDelta {
+    pub(crate) initiator: PublicKeyHash,
+    pub(crate) order: Order,
+    pub(crate) balance_deltas: Vec<AssetDelta>,
+    pub(crate) order_book_delta: OrderBookDelta,
+    pub(crate) order_deltas: 
 }
